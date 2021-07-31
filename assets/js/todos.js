e7158aa9be18785fca31f9ab0f75fd1a31e03ec2
bizlett <bizzyday@gmail.com>
2021-07-31 21:28:16 +0000

    Add basic outline structure

---
+++ assets/js/todos.js
@@ -0,0 +1,18 @@
+let todos = [];
+
+function displayTodos(){}
+
+let addButton = document.getElementById('add-button');
+let addInput = document.getElementById('add-input');
+addButton.addEventListener('click', add);
+
+function add(){}
+
+
+function edit(){}
+
+
+function remove(){}
+
+
+
