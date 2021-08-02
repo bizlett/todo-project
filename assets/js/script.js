c99ea2121c02fc6d3f1b01764166535f0e8fdef0
bizlett <bizzyday@gmail.com>
2021-08-02 11:03:04 +0000

    Working on addTodo() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,40 @@
+// add event listener
+const addButton = document.getElementById('add-button'); 
+addButton.click = addTodo;   
 
 function addTodo(){
-    // defines a variable to store new todo
-    let  todoList = document.getElementsByClassName('todo-list');
-    
-    // defines where to get the new todo from
-    let newTodo = document.getElementsByClassName('new-todo');
-    
-    // defines the element to create to store the new todo
-    let li = document.createElement('li');
+    // create a new todo
+    var newTodo = document.getElementsByClassName('new-todo');
 
-    // sets class of attribute and fetches text from input box 
-    li.setAttribute('class', newTodo.value);
+    // create a new element for the newTodo and store it in a variable
+    var li = document.createElement('li');
 
-    // appends new element into the todoList and displays text from input box
-    li.appendChild(document.createTextNode(newTodo));
+    // create a text node and store it in a variable
+    var todoText = document.createTextNode(document.getElementsByClassName('new-todo'));
+
+    // attach the new text node (todoText) to the new element (li)
+    li.appendChild(todoText);
+
+    // create the position for where the new element should be added (at the start of the ul)
+    var todoList = document.getElementById('todo-list')[0];
+
+    // insert the new element (newTodo) into the new position (start of todoList)
+    todoList.appendChild(newTodo);
 
 }
 
 
+// // defines a variable to store new todo
+// let  todoList = document.getElementsByClassName('todo-list');
+    
+// // defines where to get the new todo from
+// let newTodo = document.getElementsByClassName('new-todo');
+
+// // defines the element to create to store the new todo
+// let li = document.createElement('li');
+
+// // sets class of attribute and fetches text from input box 
+// li.setAttribute('class', newTodo.value);
+
+// // appends new element into the todoList and displays text from input box
+// li.appendChild(document.createTextNode(newTodo));
