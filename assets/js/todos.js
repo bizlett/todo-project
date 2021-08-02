7542679fd43c7c18a12cdfec80f6b9480c5c4a3b
bizlett <bizzyday@gmail.com>
2021-08-02 06:17:55 +0000

    Change html basic structure and add slider button for toggle all

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -5,7 +5,7 @@ function displayTodos(){}
 let addButton = document.getElementById('add-button');
 let addInput = document.getElementById('add-input');
 addButton.addEventListener('click', add);
-
+g
 function add(){}
 
 
