cce81132a63db88af1d877a6fd717f3ce587380d
bizlett <bizzyday@gmail.com>
2021-08-10 10:21:36 +0000

    Fix bugs on addTodo function - work in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,18 @@ document.getElementById('today-date').innerHTML = todayDate;
 const todoInput = document.querySelector('.new-todo');
 const addButton = document.querySelector('#add-button');
 const todoList = document.querySelector('#todo-list');
+    // not sure if I need declare deleteButton... Its on the todo-list
 const deleteButton = document.querySelector('.delete-button');
 
 // Event listeners
 
 addButton.addEventListener('click', addTodo);
+// funky formatting issues! want the complete button at the start and the delete button at the end ideally
+// perhaps div WITHIN the li to hold button rather than div to hold everything?
 
-todoList.addEventListener('click', deleteTodo);
+todoList.addEventListener('click', deleteTodo); 
+// this event listener is on todoList so ANY click on it will delete anything on the ul right now
+// if/else should sort this. Need to also amend to target only the li being clicked and not all of them
 
 // Functions
 
@@ -22,29 +27,43 @@ function addTodo(event) {
     // prevent form submitting
     event.preventDefault();
 
-    // create todoItem div to hold new todo and icons
-    let todoDiv = document.createElement('div');
-
-    // create new li to go into todoItem div
+    // create new li
     let newTodo = document.createElement('li');
-    newTodo.innerText = document.getElementsByClassName('new-todo')[0].value;
-    newTodo.classList.add('new-todo');
-    todoDiv.appendChild(newTodo);
 
-    // check button
+    // get todo text for new li
+    newTodo.innerText = document.getElementsByClassName('new-todo')[0].value;
+
+    // add class to style li
+    newTodo.classList.add('new-todo');
+
+    // create todoItem div to hold new complete button
+    // let todoDiv = document.createElement('div');
+
+    // create check button
     let checkButton = document.createElement('input');
-    completedButton.innerHTML = '<input type = "checkbox">';
-    completedButton.classList.add('checkbox');
-    todoDiv.appendChild(checkButton);
+
+    // write html for check button
+    checkButton.innerHTML = '<input type = "checkbox">';
+
+    // add class to style check button
+    checkButton.classList.add('checkButton');
+
+    newTodo.appendChild(checkButton);
+
+    // append div to li
+    // todoDiv.appendChild(checkButton);
+
+    // append div with checkButton inside li
+    // todoDiv.appendChild(newTodo);
 
     // delete button
     let deleteButton = document.createElement('button');
     deleteButton.innerHTML = '<i class="fas fa-times"></i>';
     deleteButton.classList.add('delete-button');
-    todoDiv.appendChild(deleteButton);
+    newTodo.appendChild(deleteButton);
 
     // append to todo list
-    todoList.appendChild(todoDiv);
+    // todoList.appendChild(todoDiv);
 
     // clear todo input
     todoInput.value = '';
@@ -52,14 +71,14 @@ function addTodo(event) {
 
 function deleteTodo(event) {
     // do I need this? look into further
-    event.preventDefault();
+    // event.preventDefault();
 
     // find the element to remove - li element that is being clicked??
-    let deleteTodo = event.target;
+    // let deleteTodo = event.target;
 
     // the containing element of the element we're removing (defined globally - might not need this here)
     // let todoList = document.querySelector('#todo-list');
 
-    todoList.remove(this.deleteTodo);
+    // todoList.remove(this.deleteTodo);
 
 };
