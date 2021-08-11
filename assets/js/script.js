22bb2337a4ded4cc46e74e18fd2f8030ba9f10e7
bizlett <bizzyday@gmail.com>
2021-08-11 05:54:22 +0000

    Add alert message for empty todoInput

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 // get date for header
-const options = {weekday: 'short', month: 'short', day: 'numeric', year: 'numeric'};
+const options = {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric'
+};
 const todayDate = new Date().toLocaleDateString('en-us', options);
 
 document.getElementById('today-date').innerHTML = todayDate;
@@ -8,7 +13,7 @@ document.getElementById('today-date').innerHTML = todayDate;
 const todoInput = document.querySelector('.new-todo');
 const addButton = document.querySelector('#add-button');
 const todoList = document.querySelector('#todo-list');
-    // not sure if I need declare deleteButton... Its on the todo-list
+// not sure if I need declare deleteButton... Its on the todo-list
 const deleteButton = document.querySelector('.delete-button');
 
 // Event listeners
@@ -17,7 +22,7 @@ addButton.addEventListener('click', addTodo);
 // funky formatting issues! want the complete button at the start and the delete button at the end ideally
 // perhaps div WITHIN the li to hold button rather than div to hold everything?
 
-todoList.addEventListener('click', deleteTodo); 
+todoList.addEventListener('click', deleteTodo);
 // this event listener is on todoList so ANY click on it will delete anything on the ul right now
 // if/else should sort this. Need to also amend to target only the li being clicked and not all of them
 
@@ -34,44 +39,46 @@ function addTodo(event) {
     // create li with text from input
     let todo = document.createElement('li');
     todo.textContent = todoInput.value;
-    
+
     // check if there is text to submit, else throw error
-    if (todo == "") {
-        alert("Please enter a task!");
-        return false;
-    };
-    
-    // append li to div
-    todoDiv.appendChild(todo);
+    if (todoInput.value != "") {
 
-    // create check button
-    let checkButton = document.createElement('input');
-    checkButton.setAttribute('type', 'checkbox');
-    checkButton.className = 'check-button';
-    todoDiv.appendChild(checkButton);
+        // append li to div
+        todoDiv.appendChild(todo);
 
-    // create delete button
-    let deleteButton = document.createElement('button'); 
-    deleteButton.innerHTML = '<i class="fas fa-times"></i>';
-    todoDiv.appendChild(deleteButton);
+        // create check button
+        let checkButton = document.createElement('input');
+        checkButton.setAttribute('type', 'checkbox');
+        checkButton.className = 'check-button';
+        todoDiv.appendChild(checkButton);
 
-    // append todoDiv to list
-    todoList.appendChild(todoDiv);
+        // create delete button
+        let deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fas fa-times"></i>';
+        todoDiv.appendChild(deleteButton);
 
-    // clear todo input
-    todoInput.value = '';
+        // append todoDiv to list
+        todoList.appendChild(todoDiv);
+
+        // clear todo input
+        todoInput.value = '';
+    } else {
+
+        // Alert message when todoInput is empty
+        alert ("Please add a task!");
+    }
 };
 
 // function deleteTodo(event) {
-    // do I need this? look into further
-    // event.preventDefault();
+// do I need this? look into further
+// event.preventDefault();
 
-    // find the element to remove - li element that is being clicked??
-    // let deleteTodo = event.target;
+// find the element to remove - li element that is being clicked??
+// let deleteTodo = event.target;
 
-    // the containing element of the element we're removing (defined globally - might not need this here)
-    // let todoList = document.querySelector('#todo-list');
+// the containing element of the element we're removing (defined globally - might not need this here)
+// let todoList = document.querySelector('#todo-list');
 
-    // todoList.remove(this.deleteTodo);
+// todoList.remove(this.deleteTodo);
 
 // };
